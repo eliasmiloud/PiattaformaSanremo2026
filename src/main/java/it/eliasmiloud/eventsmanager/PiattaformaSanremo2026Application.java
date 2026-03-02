40b43b3857b165f1605d3a95a90a0e6ea53cf325
e.miloudamar@itsprodigi.it <e.miloudamar@itsprodigi.it>
2026-03-02 14:36:01 +0100

    first commit

---
+++ src/main/java/it/eliasmiloud/eventsmanager/PiattaformaSanremo2026Application.java
@@ -0,0 +1,13 @@
+package it.eliasmiloud.eventsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiattaformaSanremo2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiattaformaSanremo2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/eliasmiloud/eventsmanager/PiattaformaSanremo2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.eliasmiloud.eventsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiattaformaSanremo2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
